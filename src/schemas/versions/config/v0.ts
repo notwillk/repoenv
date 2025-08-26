3b0e5550ba96c508ab33c638da45756e1431e066
notwillk <44582272+notwillk@users.noreply.github.com>
2025-08-26 23:42:53 +0000

    Split public and private key locations

--- src/schemas/versions/config/v0.ts
+++ src/schemas/versions/config/v0.ts
@@ -2,8 +2,15 @@ import z from 'zod';
 
 export default z.object({
   version: z.literal('v0').optional(),
-  key: z.object({
-    via: z.string(),
+  keys: z.object({
+    read_write: z.object({
+      via: z.string(),
+    }),
+    read_only: z
+      .object({
+        via: z.string(),
+      })
+      .optional(),
   }),
   redact_secrets: z.boolean().optional().default(true),
   sources: z.record(z.string(), z.array(z.string())),
