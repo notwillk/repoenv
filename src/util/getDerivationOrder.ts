71f92d3cec4af75827991a0361e1a4b46f01bada
notwillk <44582272+notwillk@users.noreply.github.com>
2025-08-26 22:52:52 +0000

    Convert to existing kahn library

---
+++ src/util/getDerivationOrder.ts
@@ -0,0 +1,35 @@
+import z from 'zod';
+import toposort from 'toposort';
+
+import VariablesSchema from '../schemas/variables';
+import extractVars from './extractVars';
+
+export function getDerivationDependencies(
+  vars: z.infer<typeof VariablesSchema>,
+): Array<[string, string | undefined]> {
+  const propertiesWithPossibleDependencies = ['derived_value', 'substitution'] as const;
+
+  const edges: Array<[string, string | undefined]> = [];
+  for (const [varName, varDef] of Object.entries(vars)) {
+    if (typeof varDef === 'string') {
+      edges.push([varName, undefined]);
+      continue;
+    }
+
+    propertiesWithPossibleDependencies.forEach((prop) => {
+      if (varDef.hasOwnProperty(prop) && (varDef as any)[prop]) {
+        const extracted = extractVars((varDef as any)[prop] as string);
+        extracted.forEach((dep) => {
+          edges.push([varName, dep]);
+        });
+      }
+    });
+  }
+
+  return edges;
+}
+
+export default function getDerivationOrder(vars: z.infer<typeof VariablesSchema>): string[] {
+  const edges = getDerivationDependencies(vars);
+  return toposort(edges);
+}
