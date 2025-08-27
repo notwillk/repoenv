f7be6a48cfcec11ee37207632df45672d6b72d7b
notwillk <44582272+notwillk@users.noreply.github.com>
2025-08-27 03:53:39 +0000

    Update key schema

--- src/schemas/versions/config/v0.ts
+++ src/schemas/versions/config/v0.ts
@@ -1,16 +1,19 @@
 import z from 'zod';
 
+const KeySchema = z.union([
+  z.object({
+    var_name: z.string(),
+  }),
+  z.object({
+    substitution: z.string(),
+  }),
+]);
+
 export default z.object({
   version: z.literal('v0').optional(),
   keys: z.object({
-    read_write: z.object({
-      via: z.string(),
-    }),
-    read_only: z
-      .object({
-        via: z.string(),
-      })
-      .optional(),
+    read_write: KeySchema,
+    read_only: KeySchema.optional(),
   }),
   redact_secrets: z.boolean().optional().default(true),
   sources: z.record(z.string(), z.array(z.string())),
